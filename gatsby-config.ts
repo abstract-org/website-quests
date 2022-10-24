552c0f6ac103c995aceb6b078c0f893ea755c0c0
Boris L <boris@agora-labs.com>
2022-10-24 13:10:15 -0700

    Added missing plugin

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,7 +9,20 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-cosmicjs`,
+      options: {
+        bucketSlug: `quests-production`, // Get this value in Bucket > Settings
+        objectTypes: [`landing-page`], // Note it will result in GraphQL queries (allCosmicjsPosts, cosmicjsPosts)
+        // If you have enabled read_key to fetch data (optional).
+        apiAccess: {
+          read_key: `930rQQJqzR2z9cJIv5Z3lFC6Gg3UrkDAlWQxVbDjQZgAHXwPdO`, // Get this value in Bucket > Settings
+        },
+        localMedia: true, // Download media locally for gatsby image (optional)
+      },
+    },
+  ],
 }
 
 export default config
